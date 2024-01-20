20b959ba95f93b2677b8c2e14936be64155a08d9
salehWeb <salehwebdev2004@gmail.com>
2024-01-20 20:14:21 +0200

    use esbuild

---
+++ entry-client.tsx
@@ -0,0 +1,4 @@
+import { hydrateRoot } from 'react-dom/client';
+import { App } from './entry-server';
+
+hydrateRoot(document, <App/>);
---
+++ pages/NotFound.tsx
@@ -0,0 +1,16 @@
+export const head = () => {
+  return (
+    <>
+      <title>404 page</title>
+      <meta rel="description" content="page not found " />
+    </>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <div>404</div>
+  )
+}
+
+export default NotFound
